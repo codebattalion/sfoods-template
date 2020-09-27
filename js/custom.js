558e9303b042dc92353f8aa1485a115b6399bfe5
codebattalion <vipanpreet.code@gmail.com>
2020-09-27 05:01:16 -0700

    Added Recipe detail page

--- js/custom.js
+++ js/custom.js
@@ -12,3 +12,34 @@ $(".nav--trigger").click(function () {
   $(".nav__bar").toggleClass("nav--open");
   $(".header").toggleClass("header--open");
 });
+
+$(document).ready(function () {
+  if ($(window).width() < 1024) {
+    let recipe_win = $(".recipe").height();
+    let recipe_inc = $(".recipe__incredients").height();
+    $(".recipe__center").css({
+      top: recipe_win + recipe_inc + parseInt(100),
+      position: "absolute",
+    });
+  } else {
+    $(".recipe__center").css({
+      top: "0",
+      position: "relative",
+    });
+  }
+});
+$(window).resize(function () {
+  if ($(window).width() < 1024) {
+    let recipe_win = $(".recipe").height();
+    let recipe_inc = $(".recipe__incredients").height();
+    $(".recipe__center").css({
+      top: recipe_win + recipe_inc + parseInt(100),
+      position: "absolute",
+    });
+  } else {
+    $(".recipe__center").css({
+      top: "0",
+      position: "relative",
+    });
+  }
+});
